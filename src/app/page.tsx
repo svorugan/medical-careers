4fc1747506bf8d6229c15d286fe24c8a7a105ab0
iTechpillar LLC <itechpillar@gmail.com>
2025-06-07 18:29:29 -0400

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
 
       {/* What You will Find Here Section */}
       <div className="mb-16">
-        <h2 className="text-3xl font-bold text-gray-800 mb-8">What You'll Find Here</h2>
+        <h2 className="text-3xl font-bold text-gray-800 mb-8">What You&apos;ll Find Here</h2>
         
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Medical Branches */}
